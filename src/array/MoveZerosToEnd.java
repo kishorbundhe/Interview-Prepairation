e24933363b46e4972bb49f66794499c0fabf9859
kishorbundhe <kishorbundhe@gmail.com>
2022-08-17 22:38:15 +0200

    Kishor | solve #3 for move zero to the end problem

--- src/array/MoveZerosToEnd.java
+++ src/array/MoveZerosToEnd.java
@@ -1,2 +1,33 @@
-package array;public class MoveZerosToEnd {
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MoveZerosToEnd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of use cases you want to perform");
+        int n = scanner.nextInt();
+        System.out.println("Enter the size of array");
+        int x = scanner.nextInt();
+        int [] array = new int[x];
+        for (int i = 0; i < x; i++) {
+            array[i] = scanner.nextInt();
+        }
+        moveZerosToEnd(array);
+    }
+
+    private static void moveZerosToEnd(int[] array) {
+        int length = array.length;
+        int indexOfCurrentPositiveNo=0;
+        for( int i=0;i<length;i++){
+            if(array[i]!=0){
+                int temp  = array[i];
+                array[i] =0;
+                array[indexOfCurrentPositiveNo] = temp;
+                indexOfCurrentPositiveNo++;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
